562034af4c033ff269297e5e28b7da5695f90b82
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-01-05 17:56:15 +0530

    Day 6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,11 +3,12 @@ import 'package:flutter_catalog/utils/routes.dart';
 
 class LoginPage extends StatefulWidget {
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,17 +22,17 @@ class _LoginPageState extends State<LoginPage> {
                 fit: BoxFit.cover,
               ),
               SizedBox(
-                height: 10.0,
+                height: 20.0,
               ),
               Text(
-                "Welcome",
+                "Welcome $name",
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               SizedBox(
-                height: 10.0,
+                height: 20.0,
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(
@@ -42,17 +43,10 @@ class _LoginPageState extends State<LoginPage> {
                       decoration: InputDecoration(
                         hintText: "Enter username",
                         labelText: "Username",
-
-                        // Box ke border ka color change
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
-                        ),
                       ),
-
-                      //to gradually change or add the name enter in (Enter Username) with Welcome TEXT
                       onChanged: (value) {
                         name = value;
-                        setState(() {});//only available in StateFul Widget
+                        setState(() {});
                       },
                     ),
                     TextFormField(
@@ -63,37 +57,47 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ),
                     SizedBox(
-                      height: 30.0,
+                      height: 40.0,
                     ),
+
                     InkWell(
-                      onTap: ()  {
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      child: Container(
-                        width: 150,
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton ? 50 : 150,
                         height: 50,
                         alignment: Alignment.center,
-                        child: Text(
+                        child: changeButton
+                            ? Icon(
+                          Icons.done,
+                          color: Colors.white,
+                        )
+                            : Text(
                           "Login",
                           style: TextStyle(
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
-                              fontSize: 18), // TextStyle
-                        ), // Text
+                              fontSize: 18),
+                        ),
                         decoration: BoxDecoration(
-                            color: Colors.deepPurple,
-                            borderRadius: BorderRadius.circular(8)
+                          color: Colors.deepPurple,
+                          borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8),
                         ),
                       ),
                     ),
-                    
+
                     // ElevatedButton(
                     //   child: Text("Login"),
                     //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
                     //   onPressed: () {
                     //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    //     // Navigator.pushNamed(context, routeName)
-                    //     // Navigator.push(context, route)
                     //   },
                     // )
                   ],
@@ -101,7 +105,6 @@ class _LoginPageState extends State<LoginPage> {
               )
             ],
           ),
-        )
-    );
+        ));
   }
 }
