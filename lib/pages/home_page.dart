0ac638ac9f0d26b7e7357d14bed92d172f5c72d4
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-01-07 15:46:53 +0530

    Day 9 theme done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 
-
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Codepur";
@@ -9,7 +8,11 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text(
+          "Catalog App",
+         // style: TextStyle(fontSize: 20, color: Colors.black),
+        ),
+        //centerTitle: true,
       ),
       body: Center(
         child: Container(
